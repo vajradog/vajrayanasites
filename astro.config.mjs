41e46a2fee0f19a8ccee2a15f63e69d3064796d2
Thupten N Chakrishar <vajradog@users.noreply.github.com>
2026-01-28 12:46:52 -0500

    Fix base URL for vajrayanasites repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://vajradog.github.io',
-  base: '/vajrayana-pilgrimage/',
+  base: '/vajrayanasites/',
   integrations: [
     tailwind(),
     mdx(),
